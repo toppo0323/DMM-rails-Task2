cf1577afb2b121c5cf1bf1c9d53e90e1ceb97df9
EC2 Default User <ec2-user@ip-172-31-22-106.ec2.internal>
2021-07-20 04:34:49 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  resources :books,only: [:new,:create,:index,:show,:destroy,:edit,:update]
+  
+end
