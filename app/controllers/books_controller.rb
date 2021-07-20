4b66009a2e20715e86d6e3bf3680741b6e7f6ff0
EC2 Default User <ec2-user@ip-172-31-22-106.ec2.internal>
2021-07-20 16:41:24 +0000

    commit2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,53 @@
 class BooksController < ApplicationController
   
-  def new
-  end
   
   def create
+    @book = Book.new(book_params)
+    
+      if @book.save
+        flash[:succes]= "You have created book successfully."
+        redirect_to book_path(@book.id)
+      else
+        @books = Book.all
+        render :index
+      end
   end
   
   def index
+    @books = Book.all
+    @book = Book.new
   end
   
   def show
+    @book = Book.find(params[:id])
   end
   
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+      if @book.save
+         flash[:succes]= "You have created book successfully."
+        redirect_to books_path(@book.id)
+      else
+        render edit
+      end
   end
   
-end
+
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books,only: [:new,:create,:index,:show,:destroy,:edit,:update]
+  resources :books
   
 end
